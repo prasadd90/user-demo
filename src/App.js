a14dc4d525789863ba0cc8904109f9616a6406d4
prasadd90 <prasadd90@gmail.com>
2026-03-02 18:03:23 +0530

    fixed for

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 import Login from "./Component/login";
 import AddStudent from "../src/Component/AddStudent";
 import StudentList from "../src/Component/StudentList"
-import Students from "./pages/Students";
+ 
 // Import CSS for styling
 function App() {
   
