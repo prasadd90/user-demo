8222c8db75f9822fe552e95da5712ac97a70158b
prasadd90 <prasadd90@gmail.com>
2026-03-04 17:25:44 +0530

    css added n div

--- src/Component/StudentList.js
+++ src/Component/StudentList.js
@@ -60,7 +60,7 @@ function StudentList({ students }) {
   const totalPages = Math.ceil(filteredStudents.length / recordsPerPage);
 
   return (
-    <div className="table-container">
+    <div className="table-container" style={{width:"100%",overflowX:"auto",borderCollapse:"collapse"}} >
           <h2 className="table-title">Student List</h2>
     
           {/* Search + Page Size */}
@@ -101,7 +101,7 @@ function StudentList({ students }) {
           </div>
     
           {/* Table */}
-          <table className="admin-table" style={{width:"100%",overflowX:"auto"}}>
+          <table className="admin-table" >
             <thead>
               <tr>
                 <th>Student Name</th>
